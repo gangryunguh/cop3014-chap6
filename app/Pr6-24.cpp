f670d63f309beff69f42f8bd461f0cb58eea713e
Gang-Ryung Uh <gangryunguh@gmail.com>
2018-11-07 16:35:59 -0600

    Update the Pr6-24 for student template

--- app/Pr6-24.cpp
+++ app/Pr6-24.cpp
@@ -25,10 +25,10 @@ void displayStars(int cols, int rows)
 {
    // Nested loop. The outer loop controls the rows
    // and the inner loop controls the columns.
-   for (int down = 0; down < rows; down++)
-   {
-      for (int across = 0; across < cols; across++)
-         cout << "*";
-      cout << endl;
-   }
+   // For cols = 4 and rows = 3
+   // The output should be
+   // ****
+   // ****
+   // ****
+
 }
